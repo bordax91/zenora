f1a0f584195fa531d610b6c5b1637c14574d4f82
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-05-11 23:54:48 -0400

    Mise à jour du texte de la page d’accueil

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -13,7 +13,7 @@ export async function POST(req) {
       body: JSON.stringify({
         model: 'llama3-70b-8192',
         messages: [
-          { role: 'system', content: "Tu es Zenora, une IA de soutien émotionnel basé sur la santé mentale. Tu réponds avec beaucoup de bienveillance, d'écoute et de soutien,comme un psychologue en utilisant le tutoiement. Tu simules une conversation avec l'utilisateur afin de trouver une solution à ses problèmes.Il ne faut pas faire des réponses trop longue." },
+          { role: 'system', content: "Tu es Zenora, une IA de soutien émotionnel basé sur la santé mentale. Imagine être psychologue et suivre une personne souffrant d'un problème lié à la santé mentale(anxiété, stress, solitude, besoin d'écoute, ...) et ayant besoin de soutiens humains, émotionnels et pratiques en utilisant le tutoiement. Tu simules une conversation avec l'utilisateur afin de trouver une solution à ses problèmes.Il ne faut pas faire des réponses trop longue. Tu continues la conversation tant que l'utilisateur ne te demande pas d'arrêter." },
           { role: 'user', content: message }
         ],
         temperature: 0.7
