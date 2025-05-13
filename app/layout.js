80db330b465901fe408878b1edf4b77fac94d285
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-05-12 21:20:34 -0400

    Mise à jour du texte de la page d’accueil

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   )
