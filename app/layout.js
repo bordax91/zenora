00fe5ad2c0f480d3b004a16a04082106cad3e1c4
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-05-13 13:32:28 -0400

    Mise à jour du texte de la page d’accueil

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,10 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
