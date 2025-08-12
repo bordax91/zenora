69b791dd20b331c6634e7168c513e33d7a5e91f6
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-08-12 14:21:40 -0400

    Ton message de commit ici

--- app/register/page.js
+++ app/register/page.js
@@ -1,4 +1,5 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic' // désactive le prerender
+'use client'
 
 import { Suspense } from 'react'
 import RegisterPageClient from './RegisterPageClient'
