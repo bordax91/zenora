8adbad4147f9a3476ecf210733f3cd811c1db2f5
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-08-18 20:42:11 -0400

    Ton message de commit ici

--- app/api/stripe/create-product/route.js
+++ app/api/stripe/create-product/route.js
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import { createClient } from '@supabase/supabase-js'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2025-07-30.basil',
 })
 
 const supabase = createClient(
