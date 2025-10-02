9278d7154f6746083de8dac991f4cff463d97ed3
Axell Bordelais <axellbordelais@MacBook-Air-de-Axell.local>
2025-10-01 23:09:56 -0400

    Ton message de commit ici

--- app/layout.js
+++ app/layout.js
@@ -94,6 +94,30 @@ export default function RootLayout({ children }) {
             alt=""
           />
         </noscript>
+
+        {/* -------------------- Reddit Pixel -------------------- */}
+        <Script
+          id="reddit-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};
+              p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;
+              var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);
+              rdt('init','a2_hrqm6spgdszv'); 
+              rdt('track','PageVisit');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.redditstatic.com/ads/pixel.gif?id=a2_hrqm6spgdszv&event=PageVisit&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
 
       <body>
